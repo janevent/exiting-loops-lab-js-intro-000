932cc69c6d8e80aa12a27cd3dd54c9efd525e10b
Jane Venter <janeanneventer@gmail.com>
2018-09-12 16:07:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,4 @@ function keepGoing(array, changeValue, skipValue){
 }
 
 1) loops findBy(array, findFn) finds the a value that satisfies `findFn`:
-     Error: Expected undefined to equal 38
-      at assert (node_modules/expect/lib/assert.js:29:9)
-      at Expectation.toEqual (node_modules/expect/lib/Expectation.js:81:30)
-      at Context.<anonymous> (test/index-test.js:25:59)
+     Error: Expected
